764c4771e17453432de74bb23bfd9c21896120cd
benikib <benikasu7@gmail.com>
2024-07-03 15:16:02 +0200

    deploiement2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,8 @@
         <link rel="icon" href="{{ Storage::url('images/Omego_logo.png')}}" type="image/png">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.min.css')}}" />
-        
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
+
 
 
         <!-- Styles -->
